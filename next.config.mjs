4a4a48e1af4e96be6c670ffcdeb3aec2775e3726
GennYoon <yoonwonyoul@webchemist.net>
2024-03-04 10:24:32 +0900

    add svg component

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   rewrites: () => {
-    return [
-      { source: "/api/:path*", destination: "http://localhost:5000/:path*" },
-    ];
+    return [{ source: "/api/:path*", destination: "http://localhost:5000/:path*" }];
   },
 };
 
